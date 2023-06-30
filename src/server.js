306257bc1dbc380b521c252efda9f6c7dfef101f
Zezheng Jiang <35544956+zjian107-su@users.noreply.github.com>
2023-06-30 16:27:38 -0400

    ✨ update: setup env to working hello world

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port localhost:${PORT}`);
+});
